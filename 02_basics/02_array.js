6c9021dae7f0f04a2297031457d067acdabeaed9
yash Tiwari <157688088+yash700701@users.noreply.github.com>
2024-11-19 23:47:52 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -20,9 +20,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
+console.log(Array.isArray("Hitesh")) f
 console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.from({name: "hitesh"})) // interesting not iterative
 
 let score1 = 100
 let score2 = 200
