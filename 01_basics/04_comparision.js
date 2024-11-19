145d90399512a074bc3d7609f026813baff99649
yash Tiwari <157688088+yash700701@users.noreply.github.com>
2024-11-19 23:29:04 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,13 +8,13 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0);  f
+console.log(null == 0);  f
+console.log(null >= 0);  t
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0);  f
+console.log(undefined > 0);  f
+console.log(undefined < 0);  f
 
 // === 
 
